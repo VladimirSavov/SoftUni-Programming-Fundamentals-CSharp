0c13d7260980a5ff4039a0bb8e3f84fa26cc6560
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-08-03 09:32:46 +0300

    Create 01.Student_Information.cs

---
+++ 2022-08-03/01.Student_Information.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName = Console.ReadLine(); 
+            int age = int.Parse(Console.ReadLine());    
+            double averageGrade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {studentName}, Age: {age}, Grade: {averageGrade:F2}");
+        }
+    }
+}
