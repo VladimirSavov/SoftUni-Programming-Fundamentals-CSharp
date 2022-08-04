c901623af783151eaeeb9ced3cf2c367d4960322
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-08-04 08:44:25 +0300

    Create 08.Divisible_by_3.cs

---
+++ 01.01. Basic Syntax, Conditional Statements and Loops - Lab/08.Divisible_by_3.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 3; i <= 100; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
