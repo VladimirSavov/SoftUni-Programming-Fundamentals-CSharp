28408fab6f57c92a3e652434091f1653fbd1739e
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-08-05 11:57:45 +0300

    Create 04.Print_and_sum.cs

---
+++ 01.02. Basic Syntax, Conditional Statements and Loops - Exercise/04.Print_and_sum.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write($"{i} ");
+                sum += i;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
