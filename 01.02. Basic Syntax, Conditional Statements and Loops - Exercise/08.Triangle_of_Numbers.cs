a603df173a04854b7b537a9c5e18100528bd966d
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-08-05 15:06:59 +0300

    Create 08.Triangle_of_Numbers.cs

---
+++ 01.02. Basic Syntax, Conditional Statements and Loops - Exercise/08.Triangle_of_Numbers.cs
@@ -0,0 +1,168 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 1; i++)
+            {
+                Console.WriteLine(1);
+            }
+            for (int l = 1; l <= 2; l++)
+            {
+                if(input >= 2)
+                {
+                Console.Write($"2 ");
+                }
+            }
+            Console.WriteLine();
+            for (int p = 1; p <= 3; p++)
+            {
+                if(input >= 3)
+                {
+                Console.Write($"3 ");
+                }
+            }
+            Console.WriteLine();
+            for (int u = 1; u <= 4; u++)
+            {
+                if (input >= 4)
+                {
+                    Console.Write($"4 ");
+                }
+            }
+            Console.WriteLine();
+            for (int w = 1; w <= 5; w++)
+            {
+                if (input >= 5)
+                {
+                    Console.Write($"5 ");
+                }
+            }
+            Console.WriteLine();
+            for (int q = 1; q <= 6; q++)
+            {
+                if (input >= 6)
+                {
+                    Console.Write($"6 ");
+                }
+            }
+            Console.WriteLine();
+            for (int a = 1; a <= 7; a++)
+            {
+                if (input >= 7)
+                {
+                    Console.Write($"7 ");
+                }
+            }
+            Console.WriteLine();
+            for (int s = 1; s <= 8; s++)
+            {
+                if (input >= 8)
+                {
+                    Console.Write($"8 ");
+                }
+            }
+            Console.WriteLine();
+            for (int d = 1; d <= 9; d++)
+            {
+                if (input >= 9)
+                {
+                    Console.Write($"9 ");
+                }
+            }
+            Console.WriteLine();
+            for (int f = 1; f <= 10; f++)
+            {
+                if (input >= 10)
+                {
+                    Console.Write($"10 ");
+                }
+            }
+            Console.WriteLine();
+            for (int g = 1; g <= 11; g++)
+            {
+                if (input >= 11)
+                {
+                    Console.Write($"11 ");
+                }
+            }
+            Console.WriteLine();
+            for (int h = 1; h <= 12; h++)
+            {
+                if (input >= 12)
+                {
+                    Console.Write($"12 ");
+                }
+            }
+            Console.WriteLine();
+            for (int z = 1; z <= 13; z++)
+            {
+                if (input >= 13)
+                {
+                    Console.Write($"13 ");
+                }
+            }
+            Console.WriteLine();
+            for (int c = 1; c <= 14; c++)
+            {
+                if (input >= 14)
+                {
+                    Console.Write($"14 ");
+                }
+            }
+            Console.WriteLine();
+            for (int v = 1; v <= 15; v++)
+            {
+                if (input >= 15)
+                {
+                    Console.Write($"15 ");
+                }
+            }
+            Console.WriteLine();
+            for (int b = 1; b <= 16; b++)
+            {
+                if (input >= 16)
+                {
+                    Console.Write($"16 ");
+                }
+            }
+            Console.WriteLine();
+            for (int n = 1; n <= 17; n++)
+            {
+                if (input >= 17)
+                {
+                    Console.Write($"17 ");
+                }
+            }
+            Console.WriteLine();
+            for (int m = 1; m <= 18; m++)
+            {
+                if (input >= 18)
+                {
+                    Console.Write($"18 ");
+                }
+            }
+            Console.WriteLine();
+            for (int t = 1; t <= 19; t++)
+            {
+                if (input >= 19)
+                {
+                    Console.Write($"19 ");
+                }
+            }
+            Console.WriteLine();
+            for (int y = 1; y <= 20; y++)
+            {
+                if (input >= 20)
+                {
+                    Console.Write($"20 ");
+                }
+            }
+        }
+    }
+}
