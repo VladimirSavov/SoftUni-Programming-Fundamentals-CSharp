d5c85a05b637500d6599e355f45d85c8fe23d32d
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-08-07 19:48:48 +0300

    Create 07.Concat_Names.cs

---
+++ 02.01. Data Types and Variables - Lab/07.Concat_Names.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOne = Console.ReadLine();
+            string nameTwo = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+            Console.WriteLine($"{nameOne}{delimiter}{nameTwo}");
+        }
+    }
+}
