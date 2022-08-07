1a427a87eb3cddd1fff3c043e1ca19dcc2746762
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-08-07 19:31:13 +0300

    Create 05.Special_Numbers.cs

---
+++ 02.01. Data Types and Variables - Lab/05.Special_Numbers.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= input; i++)
+            {
+                int num = i;
+                int sum = 0;
+                while (num > 0)
+                {
+                    sum += num % 10;
+                    num = num / 10;
+                }
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+            }
+        }
+    }
+}
