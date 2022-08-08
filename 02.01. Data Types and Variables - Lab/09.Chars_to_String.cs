58e8b9a16b5f413b8968d8d6728573a745780721
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-08-08 12:16:16 +0300

    Create 09.Chars_to_String.cs

---
+++ 02.01. Data Types and Variables - Lab/09.Chars_to_String.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbolOne = char.Parse(Console.ReadLine());
+            char symbolTwo = char.Parse(Console.ReadLine());
+            char symbolThree = char.Parse(Console.ReadLine());
+            Console.WriteLine($"{symbolOne}{symbolTwo}{symbolThree}");
+        }
+    }
+}
