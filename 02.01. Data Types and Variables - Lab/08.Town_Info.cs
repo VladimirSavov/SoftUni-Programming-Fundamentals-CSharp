e518a6caa573269322a2b5067add78bd4ddac612
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-08-08 12:15:48 +0300

    Create 08.Town_Info.cs

---
+++ 02.01. Data Types and Variables - Lab/08.Town_Info.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string townName = Console.ReadLine();
+            int population = int.Parse(Console.ReadLine());
+            double area = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Town {townName} has population of {population} and area {area} square km.");
+        }
+    }
+}
