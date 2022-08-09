89cde83238e584793b6ec9c9becf3ebeba31d8fe
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-08-10 01:13:30 +0300

    Create 03.Elevator.cs

---
+++ 02.02. Data Types and Variables - Exercise/03.Elevator.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int courses = (int)Math.Ceiling((double)numberOfPeople / capacity);
+
+            Console.WriteLine(courses);
+        }
+    }
+}
