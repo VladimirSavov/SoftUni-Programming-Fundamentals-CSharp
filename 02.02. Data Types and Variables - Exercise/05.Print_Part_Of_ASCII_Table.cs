ec46668ee4622f215346db74fdda44a49d380974
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-08-10 21:52:36 +0300

    Create 05.Print_Part_Of_ASCII_Table.cs

---
+++ 02.02. Data Types and Variables - Exercise/05.Print_Part_Of_ASCII_Table.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startChar = int.Parse(Console.ReadLine());
+            int endChar = int.Parse(Console.ReadLine());
+            for (int i = startChar; i <= endChar; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+        }
+    }
+}
