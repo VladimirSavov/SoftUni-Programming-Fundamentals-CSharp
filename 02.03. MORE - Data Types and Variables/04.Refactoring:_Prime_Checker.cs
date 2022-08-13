bde0193076d175961342446b4a4b5c94b8901e14
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-08-13 12:37:41 +0300

    Create 04.Refactoring:_Prime_Checker.cs

---
+++ 02.03. MORE - Data Types and Variables/04.Refactoring:_Prime_Checker.cs
@@ -0,0 +1,31 @@
+using System;
+namespace Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= input; i++)
+            {
+                bool isPrime = true;
+                for (int k = 2; k < i; k++)
+                {
+                    if (i % k == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.WriteLine("{0} -> true", i);
+                }
+                else
+                {
+                    Console.WriteLine("{0} -> false", i);
+                }
+            }
+        }
+    }
+}
