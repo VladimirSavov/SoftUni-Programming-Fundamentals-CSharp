57f365eeabbe6a2a35a506c29b2e362806d3066e
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-08-14 13:46:43 +0300

    Create 04.Reverse_Array_of_Strings.cs

---
+++ 03.01. Arrays - Lab/04.Reverse_Array_of_Strings.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace SoftUni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            
+            for (int i = input.Length-1; i >= 0; i--)
+            {
+                Console.Write($"{input[i]} ");
+            }
+        }
+    }
+}
