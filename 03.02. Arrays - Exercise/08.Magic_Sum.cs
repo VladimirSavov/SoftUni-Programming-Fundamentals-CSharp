f8696d4f69f5e9a67f2ffd9c56b887903fba4677
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-08-17 23:29:32 +0300

    Create 08.Magic_Sum.cs

---
+++ 03.02. Arrays - Exercise/08.Magic_Sum.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+
+namespace SoftUni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int k = i + 1; k < input.Length; k++)
+                {
+                    if (input[i] + input[k] == number)
+                    {
+                        Console.WriteLine($"{input[i]} {input[k]}");
+                    }
+                }
+            }
+        }
+        
+    }
+}
