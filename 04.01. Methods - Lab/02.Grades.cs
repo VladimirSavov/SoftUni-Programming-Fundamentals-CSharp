e50305f8cfd39d695be267b85c267e64ea0125d6
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-08-20 08:45:54 +0300

    Create 02.Grades.cs

---
+++ 04.01. Methods - Lab/02.Grades.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+
+namespace SoftUni
+{
+    internal class Program
+    {
+        static void PrintGrade(double grade)
+        {
+            if (grade >= 2 && grade <= 2.99)
+            {
+                Console.WriteLine($"Fail");
+            }
+            else if(grade >= 3 && grade <= 3.49)
+            {
+                Console.WriteLine("Poor");
+            }
+            else if(grade >= 3.5 && grade <= 4.49)
+            {
+                Console.WriteLine("Good");
+            }
+            else if(grade >= 4.5 && grade <= 5.49)
+            {
+                Console.WriteLine("Very good");
+            }
+            else
+            {
+                Console.WriteLine("Excellent");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            PrintGrade(grade);
+        }
+        
+    }
+}
