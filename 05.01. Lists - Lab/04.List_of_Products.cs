dc4cf59d96bd01308e27366db9540696b547750f
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-09-01 09:24:02 +0300

    Create 04.List_of_Products.cs

---
+++ 05.01. Lists - Lab/04.List_of_Products.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                products.Add(Console.ReadLine());
+            }
+
+            products.Sort();
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}
