49c05617eae0ea84f49bd7d3a52ef23f438fbb0e
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-09-05 23:10:17 +0300

    Create 05.Bomb_Numbers_Условие.cs

---
+++ 05.02. Lists - Exercise/05.Bomb_Numbers_Условие.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+           List<int> bombPower = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int bomb = bombPower[0];
+            int power = bombPower[1];
+
+            while (input.Contains(bomb))
+            {
+                for (int i = 0; i < power; i++)
+                {
+                    int index = input.IndexOf(bomb);
+                    if (index - 1 >= 0)
+                    {
+                        input.RemoveAt(index - 1);
+                    }
+                }
+
+                for (int i = 0; i < power; i++)
+                {
+                    int index = input.IndexOf(bomb);
+                    if (index + 1 <= input.Count - 1)
+                    {
+                        input.RemoveAt(index + 1);
+                    }
+                }
+
+                input.Remove(bomb);
+            }
+
+            Console.WriteLine(input.Sum());
+        }
+    }
+}
