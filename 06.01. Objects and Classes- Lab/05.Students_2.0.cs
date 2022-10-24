912509571c4ba94e47a09fc336c83ca0167dc49b
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2022-10-24 12:03:30 +0300

    Create 05.Students_2.0.cs

---
+++ 06.01. Objects and Classes- Lab/05.Students_2.0.cs
@@ -0,0 +1,101 @@
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.Threading;
+
+namespace SoftUni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            List<Student> output = new List<Student>();
+
+            while (command != "end")
+            {
+                List<string> input = command.Split().ToList();
+                string firstName = input[0];
+                string lastName = input[1];
+                string age = input[2];
+                string town = input[3];
+
+                Student student = new Student();
+
+                if (IsStudentExisting(output, firstName, lastName))
+                {
+
+                    student = OverWriteStudent(output, firstName, lastName, age);
+                }
+                else
+                {
+                    //   student = new Student()
+                    //   {
+                    //    FirstName = firstName,
+                    //    LastName = lastName,
+                    //    Age = age,
+                    //    Town = town,
+                    //   };
+                    student.FirstName = firstName;
+                    student.LastName = lastName;
+                    student.Age = age;
+                    student.Town = town;
+
+                    output.Add(student);
+                }
+                command = Console.ReadLine();
+            }
+            string nameOfCity = Console.ReadLine();
+
+            foreach (Student student in output)
+            {
+                if (nameOfCity == student.Town)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+        }
+
+        static bool IsStudentExisting(List<Student> output, string firstName, string lastName)
+        {
+            foreach (Student student in output)
+            {
+                if (student.FirstName == firstName && student.LastName == lastName)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static Student OverWriteStudent(List<Student> output, string firstName, string lastName, string age)
+        {
+            Student existingStudent = new Student();
+            foreach (Student student in output)
+            {
+                if (student.FirstName == firstName && student.LastName == lastName)
+                {
+                    existingStudent = student;
+                    existingStudent.Age = age;
+                }
+            }
+            return existingStudent;
+        }
+        }
+    }
+    class Student 
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Age { get; set; }
+
+        public string Town { get; set; }
+    }
+
+
+
+
